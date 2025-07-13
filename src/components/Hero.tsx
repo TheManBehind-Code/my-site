4d109982208ce217661c7d95dc867061270c24ed
TheManBehind-Code <themanbehindcode@gmail.com>
2025-07-13 22:22:54 +0200

    Add Hero Component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,20 @@
+import { motion } from "motion/react";
+
 const HeroBlock = () => {
   return (
-    <div className="bg-[#efe9e9] p-6 rounded-xl flex flex-col justify-center items-start space-y-2 h-full">
-      <p className="text-gray-600 text-sm uppercase">Hi there ! I'm</p>
-      <h1 className="ml10">
-        <span className="text-wrapper">
-          <span className="letters">Domino Dreams</span>
+    <div className="bg-[#efe9e9] flex flex-col justify-center items-center space-y-2 h-full">
+      <p className="text-gray-600 text-base uppercase">Hi there! I'm</p>
+
+      <motion.h1
+        className="text-4xl text-yellow-600"
+        initial={{ opacity: 0, x: 50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1.2 }}
+      >
+        <span className="text-wrapper" aria-hidden="true">
+          <span className="letters">Lorenzo Franceschini</span>
         </span>
-      </h1>
+      </motion.h1>
     </div>
   );
 };
